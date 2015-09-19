a07e5b516fcd2145ae97f96f8a01c440fb0eb5db
kwshoemaker2 <kyleshoe2@gmail.com>
2015-09-19 15:39:52 -0700

    finished writing unit tests for the MapGraph class

--- src/test/java/mapcolorer/MapGraphTest.java
+++ src/test/java/mapcolorer/MapGraphTest.java
@@ -18,12 +18,63 @@ public class MapGraphTest
 		testMapGraph.addNeighbors("C1", "C2");
 	}
 	
+	public void addNeighborsCheck(String id1, String id2)
+	{
+		assertEquals(String.format("{0} and {1} were not added to the map", id1, id2), 
+					true, 
+					testMapGraph.hasCountry(id1) && testMapGraph.hasCountry(id2));
+		
+		assertEquals(String.format("{0} and {1} are not neighbors", id1, id2), 
+					true,
+					testMapGraph.areNeighbors("C4", "C5"));
+	}
+	
+	@Test
+	public void addCountryTest()
+	{
+		assertEquals("C1 was not added initially", true, testMapGraph.hasCountry("C1"));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void addCountryTestShouldThrowException()
+	{
+		testMapGraph.addCountry("C1");
+	}
+	
 	@Test
 	public void addNeighborsTest()
 	{
+		// neither country is currently in the MapGraph
 		testMapGraph.addNeighbors("C4", "C5");
-		assertEquals("C4 and C5 were not added to the map", true, 
-						testMapGraph.hasCountry("C4") && testMapGraph.hasCountry("C5"));
+		addNeighborsCheck("C4", "C5");
+		
+		// one is currently in the MapGraph
+		testMapGraph.addNeighbors("C4", "C6");
+		addNeighborsCheck("C4", "C6");
+		
+		// both are currently in the MapGraph
+		testMapGraph.addNeighbors("C2", "C3");
+		addNeighborsCheck("C2", "C3");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void addNeighborsTestWithNeighbors()
+	{
+		testMapGraph.addNeighbors("C1", "C2");
+	}
+	
+	@Test
+	public void getNeighborsTest()
+	{
+		assertEquals("getNeighbors().size() should equal testMapGraph.size())",
+					 true,
+					 testMapGraph.size() == testMapGraph.getCountries().size());
+	}
+	
+	@Test
+	public void areNeighborsTest()
+	{
+		assertEquals("C1 and C2 are not neighbors!", true, testMapGraph.areNeighbors("C1", "C2"));
 	}
 }
 
